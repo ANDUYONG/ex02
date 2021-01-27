d10c3e18fc25fbe0837e2ecb9b46c0f62b5cf00a
digital11 <digital11@digital11>
2021-01-27 16:39:46 +0900

    ㅏㅏ

--- src/main/java/com/myspring/ex02/HomeController.java
+++ src/main/java/com/myspring/ex02/HomeController.java
@@ -25,7 +25,8 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		System.out.println("�ȳ�");
+		System.out.println("�ȳ�");
+		System.out.println("안ㄴ녕");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
